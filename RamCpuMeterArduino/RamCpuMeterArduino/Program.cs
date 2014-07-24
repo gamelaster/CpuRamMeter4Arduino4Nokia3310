4194693ae9761fa3d7f18edc4e29c6364ddedf90
GAMELASTER <chitrak.chitrak@gmail.com>
2014-07-24 18:26:24 +0200

    First release

---
+++ RamCpuMeterArduino/RamCpuMeterArduino/Program.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * CPU/RAM Sender via Serial Port
+ * Default maked for Arduino and Nokia 3310 display (get from phone, not buyed)
+ * By Gamiee (http://gamelaster.net/ or http://gamee.sk/
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace RamCpuMeterArduino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort sp = new SerialPort("COM5", 9600); //COM5 = port of Arduino, 9600 baudrate
+            sp.Open(); //Open SerialPort, run this after Arduino Load
+            PerformanceCounter counter = new PerformanceCounter();
+            counter.CategoryName = "Processor";
+            counter.CounterName = "% Processor Time";
+            counter.InstanceName = "_Total";
+            counter.NextValue(); //for get value (its take 0 or 100)
+
+            while (true)
+            {
+                Int64 phav = PerformanceInfo.GetPhysicalAvailableMemoryInMiB();
+                Int64 tot = PerformanceInfo.GetTotalMemoryInMiB();
+                decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
+                decimal percentOccupied = 100 - percentFree; // Small calculating, used from Library (My custom function didnt work)
+
+
+                sp.WriteLine(percentOccupied + ";" + counter.NextValue()); //sending in format "50;50", first RAM, second CPU (%)
+                Thread.Sleep(1000); //Second for next work of CpuCounter
+            }
+        }
+    }
+    //Not my Library
+    public static class PerformanceInfo
+    {
+        [DllImport("psapi.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PerformanceInformation
+        {
+            public int Size;
+            public IntPtr CommitTotal;
+            public IntPtr CommitLimit;
+            public IntPtr CommitPeak;
+            public IntPtr PhysicalTotal;
+            public IntPtr PhysicalAvailable;
+            public IntPtr SystemCache;
+            public IntPtr KernelTotal;
+            public IntPtr KernelPaged;
+            public IntPtr KernelNonPaged;
+            public IntPtr PageSize;
+            public int HandlesCount;
+            public int ProcessCount;
+            public int ThreadCount;
+        }
+
+        public static Int64 GetPhysicalAvailableMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalAvailable.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public static Int64 GetTotalMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalTotal.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+    }
+}
